8ba5efe15ee92a7308d0e086e95a174f1a948c59
aakashdeveloper <ahanda205@gmail.com>
2020-07-09 19:40:17 +0530

    a

---
+++ first.js
@@ -0,0 +1,124 @@
+dataType
+> String = "hi" 'hi' '897878' "786876" 'true' "false"
+> Number = 8476876 65 46858675857 8765.4546
+> Boolean = true/false
+
+//Es5
+var a = "hi"
+var b = 10
+var c = true
+undefined
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "6546"
+undefined
+typeof(d)
+"string"
+var e =46654.5654
+undefined
+typeof(e)
+"number"
+
+
+var a = 10
+var b = 5
+undefined
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+a%b > Modules/reminder
+0
+
+4%2
+0
+4%3
+1
+4%4
+0
+
+2%4
+2
+3%4
+3
+
+
+4%3.5
+0.5
+3.5%4
+3.5
+
+
+var a = "Hii "
+var b = "JavaScript"
+undefined
+a+b
+"Hii JavaScript"
+a-b
+NaN(Not a number)
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+var a = "Hii "
+var b = 10
+a+b
+"Hii 10"
+a-b
+NaN
+
+String+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+"10"+10
+"101010"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+
+10+"10"+10-1
+101009
+10+10+"10"-1
+2009
+"10"+10+10-1
+101009
+
+
+"10"+"10"
+"1010"
+"10"+"5"
+"105"
+"10"-"5"
+5
+"10"*"5"
+50
+"10"/"5"
+2
+"10"/"5a"
+NaN
+"10a"/"5"
+NaN
+"10"/"5"
+2
+"10"+"5"
+"105"
+"a"-5
+NaN
+"10"-5
+5
+"10"+5
+"105"
---
+++ second.js
@@ -0,0 +1,30 @@
+true is equal to 1
+false is equal to 0
+
+var a = true
+undefined
+var b = true
+undefined
+var c = false
+undefined
+a+b
+2
+a-c
+1
+a-b
+0
+true*5
+5
+true+5
+6
+true+"hi"
+"truehi"
+
+var a = "true"
+undefined
+var b = "true"
+undefined
+a+b
+"truetrue"
+a-b
+NaN
