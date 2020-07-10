606393d47de2acbdfa263edd7dd93e5b3937a29a
aakashdeveloper <ahanda205@gmail.com>
2020-07-10 19:35:25 +0530

    a

---
+++ four.js
@@ -0,0 +1,67 @@
+console.log()  >>>> Debugging
+
+alert("hiii") >>>>> notification
+alert("Form Submitted")
+
+
+confirm("Do yo want to leave")
+true
+confirm("Do yo want to leave")
+false
+var output = confirm("Do yo want to leave")
+undefined
+output
+true
+var output = confirm("Do yo want to leave")
+undefined
+output
+false
+
+prompt("what is your age")
+"10"
+prompt("what is your name")
+"aakash"
+var age = prompt("what is your age")
+undefined
+age
+"21"
+
+///////
+var first = prompt("Enter first number")
+var second = prompt("Enter Second Number")
+alert(parseFloat(first)+parseFloat(second))
+
+
+var a = "10"
+undefined
+var b = "20"
+undefined
+parseInt(a)
+10
+parseInt(b)
+20
+parseInt(a)+parseInt(b)
+30
+var c = "10a"
+undefined
+parseInt(c)
+10
+var c = "10ajjj"
+undefined
+parseInt(c)
+10
+var c = "10ajjj10"
+undefined
+parseInt(c)
+10
+var c = "a10ajjj10"
+undefined
+parseInt(c)
+NaN
+
+var a = "10.11"
+undefined
+parseInt(a)
+10
+parseFloat(a)
+10.11
---
+++ thrid.js
@@ -0,0 +1,38 @@
+=  assignment
+== compare data
+=== compare data and datatype
+
+var a  = 10
+var b = "10"
+
+
+var a  = 10
+var b = "10"
+var c = 10
+a==b
+true
+a===b
+false
+
+var c = "John"
+undefined
+var d = "john"
+undefined
+var e = "John"
+undefined
+c==d
+false
+c===d
+false
+c==e
+true
+c===e
+true
+var f = true
+undefined
+var g = "true"
+undefined
+f==g
+false
+f===g
+false
